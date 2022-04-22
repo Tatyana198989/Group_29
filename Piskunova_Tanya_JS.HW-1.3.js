3dc0340b85dbdcbc0e31aa57867140aaeff534f3
unknown <tatp89@mail.ru>
2022-04-22 10:53:01 +0300

    JS. HW-1.3

---
+++ Piskunova_Tanya_JS.HW-1.3.js
@@ -0,0 +1,30 @@
+function checkAge(age) { 
+    age = Number(age)      //Преобразовали в тип Number
+    if(!isNaN(age)) {
+    if (age < 18) {
+        console.log("You don’t have access cause your age is " + age + " It’s less then 18");
+    }
+    else if (age >= 18 && age < 60) {
+        console.log("Welcome!");
+    }
+    else if (age > 60) {
+        console.log("Keep calm and look Culture channel");
+    }
+        else{
+        console.log("Technical work")
+    }
+    }
+    else {
+        console.log("It is not a number") 
+    }
+    }
+    
+    checkAge("17")
+    checkAge("18")
+    checkAge("61")
+    checkAge('hkgd')
+    checkAge("52gjk")
+    checkAge(60)
+
+    //3**:
+//Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
